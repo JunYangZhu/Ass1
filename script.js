c3e393639f2a7f023015008768c31d63b40a188e
JunYangZhu <s10248393@connect.np.edu.sg>
2022-12-04 19:35:01 +0800

    styling and javascript

---
+++ script.js
@@ -0,0 +1,8 @@
+/*only in merch page */
+
+document.addEventListener('DOMContentLoaded', function(){
+    var imgs = document.querySelectorAll('img');
+    Array.prototype.forEach.call(imgs, function(el, i) {
+        if (el.tabIndex <= 0) el.tabIndex = 10000;
+    });
+});
